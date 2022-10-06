d80b147c555c3b256e0401ad826fb3c3f01dcadf
MAKGIS <mak-gis2@ukr.net>
2022-10-06 21:24:18 +0300

    comm 03 env.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    pimalionCloudUrl: 'http://pimsoushasvr01.dev.pimalion.cloud'
+    pimalionCloudUrl: 'https://demo.sourcing.pm/backend' //'http://pimsoushasvr01.dev.pimalion.cloud'
 };
